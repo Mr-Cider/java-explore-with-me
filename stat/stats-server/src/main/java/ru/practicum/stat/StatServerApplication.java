aa6ced2f9fc833f842f24a208710f4bb05605e04
Mr.Cider <leeone95@yandex.ru>
2025-07-30 23:38:01 +0300

    Реализован stat-service

--- stat/stats-server/src/main/java/ru/practicum/stat/StatServerApplication.java
+++ stat/stats-server/src/main/java/ru/practicum/stat/StatServerApplication.java
@@ -1,16 +1,8 @@
 package ru.practicum.stat;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 @SpringBootApplication
 @EntityScan("ru.practicum.stat.model")
