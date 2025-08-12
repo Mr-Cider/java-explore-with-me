aca730b30ac96fa2916296e2475c80f098a32678
Mr.Cider <leeone95@yandex.ru>
2025-08-12 20:23:12 +0300

    Этап 2: Попытка 1

--- ewm-main-service/src/main/java/ru/practicum/category/controller/AdminCategoryController.java
+++ ewm-main-service/src/main/java/ru/practicum/category/controller/AdminCategoryController.java
@@ -1,8 +1,9 @@
 package ru.practicum.category.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.category.dto.CategoryDto;
